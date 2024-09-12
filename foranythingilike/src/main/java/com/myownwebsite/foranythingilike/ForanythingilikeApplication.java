e33b25fdb35bbce8bfa942bf4832c91838b44e43
TiffWantToBeBad <uwchen89@gmail.com>
2024-09-12 23:51:17 +0800

    Start my work

---
+++ foranythingilike/src/main/java/com/myownwebsite/foranythingilike/ForanythingilikeApplication.java
@@ -0,0 +1,13 @@
+package com.myownwebsite.foranythingilike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForanythingilikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForanythingilikeApplication.class, args);
+	}
+
+}
---
+++ foranythingilike/src/test/java/com/myownwebsite/foranythingilike/ForanythingilikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myownwebsite.foranythingilike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForanythingilikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
